bad7e47df21a4af3849ecdd1735e51882c52bc79
Kirill <k>
2019-10-25 18:29:38 +0300

    updated Tournament form

--- htdocs/js/tournament_add.js
+++ htdocs/js/tournament_add.js
@@ -1,14 +1,51 @@
+var Tournament = {
+    init: function () {
+        this.counter = 0;
+        this.player = $(".player:last").clone().find("input").attr('value', '').attr('name', 'p_nickname[]').end();
+
+        this.deletePlayerRow();
+        this.addRow();
+        this.addRegions();
+        this.showRegionFiled();
+    },
+    deletePlayerRow: function () {
+        $(".players").on("click", ".deleteRow", function (event) {
+            $(this).closest(".player").remove();
+            Tournament.counter -= 1;
+        });
+    },
+    addRow: function () {
+        $("#addrow").on("click", function () {
+            Tournament.player.clone().appendTo(".players");
+            Tournament.counter++;
+        });
+    },
+    addRegions: function () {
+        $('#t_regions').on("blur", function (e) {
+            cities = $(e).val().trim().split(',');
+            if (Array.isArray(cities) && cities.length) {
+                for (let city of cities) {
+                    $('<option>, { value:city, text:city}').appendTo($('#p_region'));
+                }
+            }
+        });
+    },
+    showRegionFiled: function() {
+        this.toogleRegionField();
+        $('#t_type').on("change",function (e) {
+            Tournament.toogleRegionField();
+        });
+
+    },
+    toogleRegionField: function(){
+        type = +$('#t_type').children('option:selected').val();
+        if (type === 2) {
+            $('#regions_row,.p_region').show();
+        } else {
+            $('#regions_row,.p_region').hide();
+        }
+    },
+};
 $(document).ready(function () {
-    var counter = 0;
-    var player = $(".player:last").clone().find("input").attr('value','').attr('name','p_nickname[]').end();
-
-    $("#addrow").on("click", function () {
-        player.clone().appendTo(".players");
-        counter++;
-    });
-
-    $(".players").on("click", ".deleteRow", function (event)    {
-        $(this).closest(".player").remove();
-        counter -= 1;
-    });
+    Tournament.init();
 });
